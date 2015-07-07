cfc2476c0c1db83f419cc6748b22601e82739e23
errnio <errnio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 14:06:13 +0000

    Refactored php method names, changed post method,  refactored const names, refactored ajax js+php bridge method names
    git-svn-id: https://plugins.svn.wordpress.org/swipy-by-errnio/trunk@1194116 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- swipy-by-errnio.php
+++ swipy-by-errnio.php
@@ -10,66 +10,67 @@ Author URI: http://errnio.com
 
 /***** Constants ******/
 
-define('ERRNIO_INSTALLER_NAME', 'wordpress_swipy_by_errnio');
+define('SWIPY_BY_ERRNIO_INSTALLER_NAME', 'wordpress_swipy_by_errnio');
 
-define('ERRNIO_OPTION_NAME_TAGID', 'errnio_id');
-define('ERRNIO_OPTION_NAME_TAGTYPE', 'errnio_id_type');
+define('SWIPY_BY_ERRNIO_OPTION_NAME_TAGID', 'errnio_id');
+define('SWIPY_BY_ERRNIO_OPTION_NAME_TAGTYPE', 'errnio_id_type');
 
-define('ERRNIO_EVENT_NAME_ACTIVATE', 'wordpress_activated');
-define('ERRNIO_EVENT_NAME_DEACTIVATE', 'wordpress_deactivated');
-define('ERRNIO_EVENT_NAME_UNINSTALL', 'wordpress_uninstalled');
+define('SWIPY_BY_ERRNIO_EVENT_NAME_ACTIVATE', 'wordpress_activated');
+define('SWIPY_BY_ERRNIO_EVENT_NAME_DEACTIVATE', 'wordpress_deactivated');
+define('SWIPY_BY_ERRNIO_EVENT_NAME_UNINSTALL', 'wordpress_uninstalled');
 
-define('ERRNIO_TAGTYPE_TEMP', 'temporary');
-define('ERRNIO_TAGTYPE_PERM', 'permanent');
+define('SWIPY_BY_ERRNIO_TAGTYPE_TEMP', 'temporary');
+define('SWIPY_BY_ERRNIO_TAGTYPE_PERM', 'permanent');
 
 /***** Utils ******/
 
-function errnio_do_curl_request($url, $data) {
-	$data = json_encode( $data );
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	$response = curl_exec($ch);
-	curl_close($ch);
-	return json_decode($response);
+function swipy_by_errnio_do_post_request($url, $data) {
+    $data = json_encode( $data );
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/json',
+            'content' => $data
+        )
+    );
+    $context = stream_context_create($opts);
+    $response = file_get_contents($url, false, $context);
+    return json_decode($response);
 }
 
-function errnio_send_event($eventType) {
-	$tagId = get_option(ERRNIO_OPTION_NAME_TAGID);
+function swipy_by_errnio_send_event($eventType) {
+	$tagId = get_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID);
 	if ($tagId) {
 		$urlpre = 'http://customer.errnio.com';
 	 	$createTagUrl = $urlpre.'/sendEvent';
 
 	 	$params = array('tagId' => $tagId, 'eventName' => $eventType);
-	 	$response = errnio_do_curl_request($createTagUrl, $params);
+	 	$response = swipy_by_errnio_do_post_request($createTagUrl, $params);
 	}
 	// No tagId - no point sending an event
 }
 
-function errnio_mobile_gestures_create_tagid() {
+function swipy_by_errnio_create_tagid() {
 	$urlpre = 'http://customer.errnio.com';
  	$createTagUrl = $urlpre.'/createTag';
- 	$params = array('installerName' => ERRNIO_INSTALLER_NAME);
- 	$response = errnio_do_curl_request($createTagUrl, $params);
+ 	$params = array('installerName' => SWIPY_BY_ERRNIO_INSTALLER_NAME);
+ 	$response = swipy_by_errnio_do_post_request($createTagUrl, $params);
 
 	if ($response && $response->success) {
 		$tagId = $response->tagId;
-		add_option(ERRNIO_OPTION_NAME_TAGID, $tagId);
-	 	add_option(ERRNIO_OPTION_NAME_TAGTYPE, ERRNIO_TAGTYPE_TEMP);
+		add_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID, $tagId);
+	 	add_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGTYPE, SWIPY_BY_ERRNIO_TAGTYPE_TEMP);
 		return $tagId;
 	}
 	
 	return NULL;
 }
 
-function errnio_check_need_register() {
-	$tagtype = get_option(ERRNIO_OPTION_NAME_TAGTYPE);
+function swipy_by_errnio_check_need_register() {
+	$tagtype = get_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGTYPE);
 	$needregister = true;
 	
-	if ($tagtype == ERRNIO_TAGTYPE_PERM) {
+	if ($tagtype == SWIPY_BY_ERRNIO_TAGTYPE_PERM) {
 		$needregister = false;
 	}
 	
@@ -78,49 +79,49 @@ function errnio_check_need_register() {
 
 /***** Activation / Deactivation / Uninstall hooks ******/
 
-function errnio_mobile_gestures_activate() {
+function swipy_by_errnio_activate() {
 	if ( ! current_user_can( 'activate_plugins' ) )
 	        return;
 	
-	$tagId = get_option(ERRNIO_OPTION_NAME_TAGID);
+	$tagId = get_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID);
 
 	if ( $tagId === FALSE || empty($tagId) ) {
 		// First time activation
-		$tagId = errnio_mobile_gestures_create_tagid();
+		$tagId = swipy_by_errnio_create_tagid();
 	} else {
 		// Previously activated - meaning tagType + tagId should exists
 	}
 	
 	// Send event - activated
-	errnio_send_event(ERRNIO_EVENT_NAME_ACTIVATE);
+	swipy_by_errnio_send_event(SWIPY_BY_ERRNIO_EVENT_NAME_ACTIVATE);
 }
 
-function errnio_mobile_gestures_deactivate() {
+function swipy_by_errnio_deactivate() {
 	if ( ! current_user_can( 'activate_plugins' ) )
 	        return;
 	
 	// Send event - deactivated
-	errnio_send_event(ERRNIO_EVENT_NAME_DEACTIVATE);
+	swipy_by_errnio_send_event(SWIPY_BY_ERRNIO_EVENT_NAME_DEACTIVATE);
 }
 
-function errnio_mobile_gestures_uninstall() {
+function swipy_by_errnio_uninstall() {
 	if ( ! current_user_can( 'activate_plugins' ) )
 	        return;
 
 	// Send event - uninstall
-	errnio_send_event(ERRNIO_EVENT_NAME_UNINSTALL);	
+	swipy_by_errnio_send_event(SWIPY_BY_ERRNIO_EVENT_NAME_UNINSTALL);	
 	
-	delete_option(ERRNIO_OPTION_NAME_TAGID);
-	delete_option(ERRNIO_OPTION_NAME_TAGTYPE);
+	delete_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID);
+	delete_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGTYPE);
 }
 
-register_activation_hook( __FILE__, 'errnio_mobile_gestures_activate' );
-register_deactivation_hook( __FILE__, 'errnio_mobile_gestures_deactivate' );
-register_uninstall_hook( __FILE__, 'errnio_mobile_gestures_uninstall' );
+register_activation_hook( __FILE__, 'swipy_by_errnio_activate' );
+register_deactivation_hook( __FILE__, 'swipy_by_errnio_deactivate' );
+register_uninstall_hook( __FILE__, 'swipy_by_errnio_uninstall' );
 
 /***** Client side script load ******/
 
-function errnio_mobile_gestures_load_client_script() {
+function swipy_by_errnio_load_client_script() {
 	$list = 'enqueued';
 	$handle = 'errnio_script';
 
@@ -129,10 +130,10 @@ function errnio_mobile_gestures_load_client_script() {
 		return;
 	}
 
-	$tagId = get_option(ERRNIO_OPTION_NAME_TAGID);
+	$tagId = get_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID);
 
 	if (!$tagId || empty($tagId)) {
-		$tagId = errnio_mobile_gestures_create_tagid();
+		$tagId = swipy_by_errnio_create_tagid();
 	}
 
 	if ($tagId) {
@@ -142,7 +143,7 @@ function errnio_mobile_gestures_load_client_script() {
 	}
 }
 
-function errnio_mobile_gestures_load_client_script_add_async_attr( $url ) {
+function swipy_by_errnio_load_client_script_add_async_attr( $url ) {
 	if(FALSE === strpos( $url, 'service2.errnio.com')){
 		return $url;
 	}
@@ -150,23 +151,23 @@ function errnio_mobile_gestures_load_client_script_add_async_attr( $url ) {
 	return "$url' async='async";
 }
 
-add_filter('clean_url', 'errnio_mobile_gestures_load_client_script_add_async_attr', 11, 1);
-add_action('wp_enqueue_scripts', 'errnio_mobile_gestures_load_client_script', 99999 );
+add_filter('clean_url', 'swipy_by_errnio_load_client_script_add_async_attr', 11, 1);
+add_action('wp_enqueue_scripts', 'swipy_by_errnio_load_client_script', 99999 );
 
 /***** Admin ******/
 
-function errnio_mobile_gestures_add_settings_menu_option() {
+function swipy_by_errnio_add_settings_menu_option() {
     add_menu_page (
         'Errnio Options',
         'Errnio Settings',
         'manage_options',
         'errnio-options',
-        'errnio_mobile_gestures_admin_page',
+        'swipy_by_errnio_admin_page',
 		'dashicons-smartphone'
     );
 }
 
-function errnio_mobile_gestures_add_settings_link_on_plugin($links, $file) {
+function swipy_by_errnio_add_settings_link_on_plugin($links, $file) {
     static $this_plugin;
 
     if (!$this_plugin) {
@@ -182,8 +183,8 @@ function errnio_mobile_gestures_add_settings_link_on_plugin($links, $file) {
     return $links;
 }
 
-function errnio_mobile_gestures_admin_notice() {
-	$needregister = errnio_check_need_register();
+function swipy_by_errnio_admin_notice() {
+	$needregister = swipy_by_errnio_check_need_register();
 	$settingsurl = admin_url( 'admin.php?page=errnio-options' );
 	
 	if($needregister){
@@ -191,7 +192,7 @@ function errnio_mobile_gestures_admin_notice() {
 	}
 }
 
-function errnio_mobile_gestures_admin_page() {
+function swipy_by_errnio_admin_page() {
 	$stylehandle = 'errnio-style';
 	$jshandle = 'errnio-js';
 	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Exo+2:700,400,200,700italic,300italic,300');
@@ -204,8 +205,8 @@ function errnio_mobile_gestures_admin_page() {
     ?>
     <div class="wrap">
 		<?php
-		$needregister = errnio_check_need_register();
-		$tagId = get_option(ERRNIO_OPTION_NAME_TAGID);
+		$needregister = swipy_by_errnio_check_need_register();
+		$tagId = get_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID);
 
 		echo '<h2>Errnio Options</h2>';
 
@@ -213,7 +214,7 @@ function errnio_mobile_gestures_admin_page() {
 			echo '<div class="bold"><p>Your new errnio plugin is up and running.<br/>For configuration and reports please visit your dashboard at <a href="http://brutus.errnio.com/">brutus.errnio.com</a></p><br/><img src="'.plugins_url('assets/img/logo-366x64.png', __FILE__).'"/></div>';
 		} else {
 			if ($tagId) {
-				echo '<div class="errnio" height="100%" width="100%" data-tagId="'.$tagId.'" data-installName="'.ERRNIO_INSTALLER_NAME.'">';
+				echo '<div class="errnio" height="100%" width="100%" data-tagId="'.$tagId.'" data-installName="'.SWIPY_BY_ERRNIO_INSTALLER_NAME.'">';
 				include 'assets/includes/errnio-admin.php';
 				echo '</div>';
 			} else {
@@ -226,20 +227,20 @@ function errnio_mobile_gestures_admin_page() {
     <?php
 }
 
-function errnio_register_callback() {
+function swipy_by_errnio_register_callback() {
 	$type = $_POST['type'];
 	$tagId = $_POST['tag_id'];
 
 	if ($type == 'switchTag') {
-		update_option(ERRNIO_OPTION_NAME_TAGID, $tagId);
+		update_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGID, $tagId);
 	}
 
-	update_option(ERRNIO_OPTION_NAME_TAGTYPE, ERRNIO_TAGTYPE_PERM);
+	update_option(SWIPY_BY_ERRNIO_OPTION_NAME_TAGTYPE, SWIPY_BY_ERRNIO_TAGTYPE_PERM);
 
 	wp_die(); // this is required to terminate immediately and return a proper response
 }
 
-add_action('admin_menu', 'errnio_mobile_gestures_add_settings_menu_option');
-add_filter('plugin_action_links', 'errnio_mobile_gestures_add_settings_link_on_plugin', 10, 2);
-add_action('admin_notices', 'errnio_mobile_gestures_admin_notice');
-add_action('wp_ajax_errnio_register', 'errnio_register_callback');
+add_action('admin_menu', 'swipy_by_errnio_add_settings_menu_option');
+add_filter('plugin_action_links', 'swipy_by_errnio_add_settings_link_on_plugin', 10, 2);
+add_action('admin_notices', 'swipy_by_errnio_admin_notice');
+add_action('wp_ajax_swipy_by_errnio_register', 'swipy_by_errnio_register_callback');
