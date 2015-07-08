cfb8a20046f48d5828a19f059f2680423516d485
errnio <errnio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 11:26:23 +0000

    Tagging 1.1 version release
    git-svn-id: https://plugins.svn.wordpress.org/swipy-by-errnio/trunk@1194695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- swipy-by-errnio.php
+++ swipy-by-errnio.php
@@ -3,7 +3,7 @@
 Plugin Name: Swipy by errnio
 Plugin URI: http://errnio.com
 Description: Swipy offers your mobile site visitors more of your content with swipe action cards, enhancing experience, content circulation and time spent.
-Version: 1.0
+Version: 1.1
 Author: Errnio
 Author URI: http://errnio.com
 */
@@ -24,18 +24,15 @@ define('SWIPY_BY_ERRNIO_TAGTYPE_PERM', 'permanent');
 
 /***** Utils ******/
 
-function swipy_by_errnio_do_post_request($url, $data) {
-    $data = json_encode( $data );
-    $opts = array('http' =>
-        array(
-            'method'  => 'POST',
-            'header'  => 'Content-type: application/json',
-            'content' => $data
-        )
-    );
-    $context = stream_context_create($opts);
-    $response = file_get_contents($url, false, $context);
-    return json_decode($response);
+function swipy_by_errnio_do_wp_post_request($url, $data) {
+	$data = json_encode( $data );
+	$header = array('Content-type' => 'application/json');
+	$response = wp_remote_post($url, array(
+	    'headers' => $header,
+	    'body' => $data
+	));
+
+	return json_decode(wp_remote_retrieve_body($response));
 }
 
 function swipy_by_errnio_send_event($eventType) {
@@ -45,7 +42,7 @@ function swipy_by_errnio_send_event($eventType) {
 	 	$createTagUrl = $urlpre.'/sendEvent';
 
 	 	$params = array('tagId' => $tagId, 'eventName' => $eventType);
-	 	$response = swipy_by_errnio_do_post_request($createTagUrl, $params);
+	 	$response = swipy_by_errnio_do_wp_post_request($createTagUrl, $params);
 	}
 	// No tagId - no point sending an event
 }
@@ -54,7 +51,7 @@ function swipy_by_errnio_create_tagid() {
 	$urlpre = 'http://customer.errnio.com';
  	$createTagUrl = $urlpre.'/createTag';
  	$params = array('installerName' => SWIPY_BY_ERRNIO_INSTALLER_NAME);
- 	$response = swipy_by_errnio_do_post_request($createTagUrl, $params);
+ 	$response = swipy_by_errnio_do_wp_post_request($createTagUrl, $params);
 
 	if ($response && $response->success) {
 		$tagId = $response->tagId;
